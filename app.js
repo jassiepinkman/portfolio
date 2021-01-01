41b87814e65c270be0818f432ae090e9e09ae40f
Jaskaran Singh <52860189+jassiepinkman@users.noreply.github.com>
2021-01-02 01:25:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,14 +26,6 @@ function linkAction(){
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 500,
-    reset: true
-});
-
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
